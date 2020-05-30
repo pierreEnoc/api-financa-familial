e7a2333fd8895e6ed071e3ba7eb63908bc78d24b
pierreEnoc <pierre_enoc@yahoo.fr>
2020-05-30 19:32:17 -0300

    api finanaca familial

---
+++ src/main/java/com/pierre/minhasfinancas/api/dto/AtualizaStatusDTO.java
@@ -0,0 +1,13 @@
+package com.pierre.minhasfinancas.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AtualizaStatusDTO {
+
+	private String status;
+}
